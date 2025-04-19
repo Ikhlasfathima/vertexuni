37e15b95411eea8e9f4e99ef5d502753f516c69d
Ikhlasfathima <ikhlasfathima07@gmail.com>
2025-04-20 03:16:55 +0530

    updated many things

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ const slides = document.querySelectorAll('.slide');
     current = (current - 1 + slides.length) % slides.length;
     slides[current].classList.add('active');
   });
+
+  function openLightbox(imageId) {
+    var lightbox = document.getElementById("lightbox");
+    var lightboxImg = document.getElementById("lightbox-img");
+    
+    // Get the clicked image source
+    var imgSrc = document.getElementById(imageId).src;
+    
+    // Set the lightbox image source to the clicked image's source
+    lightboxImg.src = imgSrc;
+    
+    // Display the lightbox
+    lightbox.style.display = "flex";
+  }
+  
+  function closeLightbox() {
+    var lightbox = document.getElementById("lightbox");
+    
+    // Hide the lightbox
+    lightbox.style.display = "none";
+  }
+  
